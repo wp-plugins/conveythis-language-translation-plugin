e18e778e3f82a12e77508a6eaed9a8905712a2db
conveythis <conveythis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-14 20:06:58 +0000

    more updates
    git-svn-id: https://plugins.svn.wordpress.org/conveythis-language-translation-plugin/tags/2.0@86687 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/conveythis.php
@@ -0,0 +1,131 @@
+<?php
+/*
+Plugin Name: ConveyThis Language Translation Plugin
+Plugin URI: http://www.conveythis.com
+Description: Allows your users to translate your blog into many different languages. The button is added to the bottom of every post.
+Author: ConveyThis.com
+Version: 1.0
+Author URI: http://www.conveythis.com
+*/
+/*  Copyright 2008  ConveyThis.com  (email : mike@conveythis.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+class ConveyThisWidget {
+	//==========================================
+	// Want stats with your ConveyThis plugin? 
+	// Replace 'wordpress' with your ConveyThis
+	// username.
+	
+	// i.e. if your name is testing, the line
+	// should read:
+	// var $conveythis_username = 'testing';
+	
+	// Also, if your page is not written in
+	// English, replace your language below.
+	// The possible language list is:
+	/*  "Arabic"
+		"Brazilian Portuguese"
+		"Bulgarian"
+		"Chinese"
+		"Chinese-simp"
+		"Chinese-trad"
+		"Croatian"
+		"Czech"
+		"Danish"
+		"Dutch"
+		"English"
+		"Finnish"
+		"French"
+		"German"
+		"Greek"
+		"Hindi"
+		"Hungarian"
+		"Icelandic"
+		"Italian"
+		"Japanese"
+		"Korean"
+		"Norwegian"
+		"Polish"
+		"Portuguese"
+		"Romanian"
+		"Russian"
+		"Serbian"
+		"Slovenian"
+		"Spanish"
+		"Swedish"
+		"Welsh"
+	*/
+	// i.e. if your page is written in Spanish,
+	// then the line:
+	// var $conveythis_source = 'English';
+	// Should be changed to:
+	// var $conveythis_source = 'Spanish';
+	
+	var $conveythis_username = 'wordpress';
+	var $conveythis_source = 'English';
+	//==========================================
+	
+	// Constructor
+	function ConveyThisWidget(){
+		add_filter('the_content', array(&$this, 'codeToContent'));
+	}
+	
+	function codeToContent($content){  
+		$link  = urlencode(get_permalink());
+		// alert the link here, to test.
+		return $content.$this->getConveyThisCode($link);
+	}
+	
+	// Get the actual button code
+	function getConveyThisCode($link) {
+		$convey_code = '<script type="text/javascript">';
+		$convey_code .=	'convey_source   = "'.$this->conveythis_source.'";';
+		$convey_code .=	'convey_user     = "'.$this->conveythis_username.'";';
+		$convey_code .=	'</script>';
+		$convey_code .=	'<a href="http://www.translation-services-usa.com/" id="conveythis_image" title="translation services" onmouseover="conveythis_show(this)" onmouseout="conveythis_start_timer()" onclick="return conveythis_prepWindow(this)" ><img src="http://e1.conveythis.com/kern_e1/images/translate1.gif" style="border-style: none;" /></a>';
+		$convey_code .=	'<script type="text/javascript" src="http://e1.conveythis.com/kern_e1/javascript/e1_1.js"></script>';        
+        return $convey_code;
+	}
+	
+	function ConveyThisWidget_doposts($content){  
+		for ($i=0; $i<10; $i++){
+			$content .= $this->ConveyThisWidget_post($i);
+		}
+		return $content;
+	}
+	
+	function ConveyThisWidget_post($entry){
+		$link  = urlencode(get_permalink());
+		if (!isset($link)){
+			$widget_post  = $this->getConveyThisCode($link);
+			$widget_post .= $this->ConveyThisWidget_postit($entry);
+		}
+		return $widget_post;
+	}
+	
+	function ConveyThisWidget_postit($i){
+		add_filter('the_content', array(&$this, 'codeToContent'));
+		$content = $this->codeToContent($content);
+		
+		$cut = explode("|", $content);
+		$post = $cut[0];
+		$link  = $cut[1]; 
+		return content . "<br />$link";
+	}
+}
+
+$convey_this &= new ConveyThisWidget();
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,29 @@
+=== Plugin Name ===
+Contributors: conveythis
+Tags: conveythis, translation, free, translator
+Tested up to: 2.6
+Stable tag: 2.0
+
+Allows your users to translate your blog into many different languages. The button is added to the bottom of every post.
+
+== Description ==
+
+Free Translation widget provided by [ConveyThis](http://www.conveythis.com/ "free translation"). ConveyThis combines Google Translator 
+Live Translator, Babel Fish, and many other people online translators into one small, easy to use button. 
+Once installed, the button will appear at the bottom of every post on the main page, as well as at the bottom of 
+the individual posts.
+
+ConveyThis supports over 30 languages, and we will be adding more soon.
+
+Please do not modify the code or link information, or the button may not display properly.
+
+== Installation ==
+
+1. Upload conveythis-language-translation-plugin to the /wp-content/plugins/ directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Coming soon... =
+
+Coming soon....
