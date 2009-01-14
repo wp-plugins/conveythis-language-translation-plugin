31c929cc181b762482a825c228ac9efa73b3d73b
conveythis <conveythis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-14 21:05:54 +0000

    added version 1.0 back in
    git-svn-id: https://plugins.svn.wordpress.org/conveythis-language-translation-plugin/tags/2.0@86699 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- conveythis.php
+++ conveythis.php
@@ -4,7 +4,7 @@ Plugin Name: ConveyThis Language Translation Plugin
 Plugin URI: http://www.conveythis.com
 Description: Allows your users to translate your blog into many different languages. The button is added to the bottom of every post.
 Author: ConveyThis.com
-Version: 1.0
+Version: 2.0
 Author URI: http://www.conveythis.com
 */
 /*  Copyright 2008  ConveyThis.com  (email : mike@conveythis.com)
